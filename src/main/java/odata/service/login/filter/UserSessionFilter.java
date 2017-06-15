6ace616a9d5c2be51cd2c518bf7639f61a80dac7
oracion1107 <oracion1107@vip.qq.com>
2017-06-15 16:46:45 +0800

    刷新缓存拦截器

--- src/main/java/odata/service/login/filter/UserSessionFilter.java
+++ src/main/java/odata/service/login/filter/UserSessionFilter.java
@@ -3,6 +3,7 @@ package odata.service.login.filter;
 
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -10,9 +11,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.apache.cxf.helpers.IOUtils;
+
+import com.alibaba.fastjson.JSONObject;
+
 import odata.service.login.util.JWTFactory;
 import odata.service.tool.StringUtil;
-
 import persistence.User;
 
 public class UserSessionFilter implements Filter {
@@ -24,19 +28,25 @@ public class UserSessionFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		String token = request.getParameter(JWTFactory.session_name);
-		
-		if(!StringUtil.isEmpty(token) && !StringUtil.isEmpty(JWTFactory.getJWT_USER(token))){
-			User user = new User();
-	    	
-	    	user.setUser_id(JWTFactory.getJWT_USER(token));
-	    	String customerId = JWTFactory.getJWT_CUSTOMER(token);
-	    	
-	    	JWTFactory.getJWT(user, customerId);
-		}
+//		String json = IOUtils.toString(request.getInputStream());
+//		
+//		if(!StringUtil.isEmpty(json)) {
+//			JSONObject jsonObject = JSONObject.parseObject(json);
+//			String token = jsonObject.getString(JWTFactory.session_name);
+//			
+//			if(!StringUtil.isEmpty(token) && !StringUtil.isEmpty(JWTFactory.getJWT_USER(token))){
+//				User user = new User();
+//		    	
+//		    	user.setUser_id(JWTFactory.getJWT_USER(token));
+//		    	String customerId = JWTFactory.getJWT_CUSTOMER(token);
+//		    	
+//		    	JWTFactory.getJWTByCustId(user, customerId);
+//			}
+//		}
 		
 		chain.doFilter(request, response);
 	}
+	
 
 	public void destroy() {
 		// TODO Auto-generated method stub
